f366b108373dcf965c11533983a6bc9cc52ef440
sohaib <waytosohaib@gmail.com>
2024-01-22 18:57:33 +0500

    first jani

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb://localhost:27017/your-database-name',
+  'mongodb+srv://sohaibafzal:M90nSfPxSrDMbOJ3@cluster0.2ufk0nv.mongodb.net/your-database-name',
   { useNewUrlParser: true, useUnifiedTopology: true },
   console.log('Database Connected')
 );
